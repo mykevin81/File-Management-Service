472bc9b7c727c877c8bb2046ce812cc778bf55a8
mykevin81 <mykevin81@gmail.com>
2023-04-03 19:58:17 -0700

    Add core and api

---
+++ file-management-service-core/src/main/java/com/serverautomation/filemanagementservicecore/FileManagementServiceCoreApplication.java
@@ -0,0 +1,13 @@
+package com.serverautomation.filemanagementservicecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagementServiceCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileManagementServiceCoreApplication.class, args);
+    }
+
+}
---
+++ file-management-service-core/src/test/java/com/serverautomation/filemanagementservicecore/FileManagementServiceCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serverautomation.filemanagementservicecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileManagementServiceCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
