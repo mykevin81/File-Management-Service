04d762be15189085f7616e4c3205d5236cf259f4
mykevin81 <mykevin81@gmail.com>
2023-04-04 22:35:56 -0700

    fix submodule and update build gradle

---
+++ file-management-service-api/src/main/java/com/serverautomation/filemanagementserviceapi/FileManagementServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.serverautomation.filemanagementserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagementServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManagementServiceApiApplication.class, args);
+	}
+
+}
---
+++ file-management-service-api/src/test/java/com/serverautomation/filemanagementserviceapi/FileManagementServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serverautomation.filemanagementserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileManagementServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
